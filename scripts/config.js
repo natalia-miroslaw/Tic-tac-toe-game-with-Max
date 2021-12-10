94921607d33d208d4d4fd5b97ffb00bb4c184fce
natalia <n.miroslaw93@gmail.com>
2021-12-10 18:55:36 +0100

    feat (.js): add open/close player config function

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
+
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+}
