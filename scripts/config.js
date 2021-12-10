49aad14e39345ae8950222906cf30c1d41c5e4f9
natalia <n.miroslaw93@gmail.com>
2021-12-10 22:49:08 +0100

    feat (.js): add user name validation

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,33 @@
 function openPlayerConfig() {
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
-
 }
 
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  //zapobiega automatycznemu wysyłaniu formularza do serwera
+  //i odświeżaniu strony (bo przeglądarka nie wie, co innego z tym zrobić)
+
+  const formData = new FormData(event.target);
+  //FormData - instantiating an object based on an object blueprint
+  //knows how to generate objects that have a certain shape
+  //takes a form (HTML elem.) and then automatically extract values entered into inputs in that form
+  //JS automatycznie będzie szukał inputs w formularzu (którę mają name), dzięki Form Data
+
+  const enteredPlayerName = formData.get('playername').trim();
+  //get value; name att., nie id!!
+  //trim usuwa nadmiar white space - '     Max      ' => 'Max'
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;                                 //return stops executing a function!
+  }
 }
