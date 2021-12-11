66f508f34d86cdcde38f5933db9fb001ba8a29ec
natalia <n.miroslaw93@gmail.com>
2021-12-11 22:09:09 +0100

    feat (.js): add place mark and switch turns functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,24 @@
 function startNewGame() {
   if (!players[0].name || !players[1].name) {
     alert('Please set custom player names for both players!');
-  } else {
-    gameAreaElement.style.display = 'block';
+    return;
   }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  event.target.textContent = players[activePlayer].symbol;   //na starcie gry pierwszy gracz [0]
+  event.target.classList.add('disabled');
+  switchPlayer();
+
 }
